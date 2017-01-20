db4adaebc28381c6bdc1a3318f0c0be95cf12080
fellyph <alanlongmore@gmail.com>
2017-01-20 12:00:19 +0000

    remove cursor pointer

--- js/mobile.js
+++ js/mobile.js
@@ -1,8 +1,9 @@
 (function() {
-    "use strict";
+    'use strict';
     jQuery(document).ready(function(){ 
-        if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-           jQuery('a[href^="tel:"]').on('click', function(e){
+        if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+           jQuery('a[href^="tel:"]').css('cursor','default')
+            .on('click', function(e){
                 e.preventDefault();
             });
         }
