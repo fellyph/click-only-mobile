777fb985dd384fac84e5cde3868ef0170ec0b6a9
fellyph <fellyph.cintra@gmail.com>
2017-01-20 12:09:56 +0000

    adding shorthand for ready event

--- js/mobile.js
+++ js/mobile.js
@@ -1,11 +1,11 @@
-(function() {
+(function($) {
     'use strict';
-    jQuery(document).ready(function(){ 
-        if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-           jQuery('a[href^="tel:"]').css('cursor','default')
+    $(function() {
+        if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+           $('a[href^="tel:"]').css('cursor','default')
             .on('click', function(e){
                 e.preventDefault();
             });
         }
     });
-})();
+})(jQuery);
